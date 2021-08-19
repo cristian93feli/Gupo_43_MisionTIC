05f68dc6923cf28031e0c7f5a5b2cd32e0ecbf6b
cristian93feli <86752849+cristian93feli@users.noreply.github.com>
2021-08-18 21:32:20 -0500

    Add files via upload
    reto1

---
+++ ClassPrincipal/src/classprincipal/ClassPrincipal.java
@@ -0,0 +1,17 @@
+
+package classprincipal;
+
+
+public class ClassPrincipal {
+
+ 
+    public static void main(String[] args) {
+       
+        PersonaClass inst_Persona = new PersonaClass("Carlos",1058965412,1.74f,28,'M',true,84.5f,"cra 54 # 14-12");
+        
+        System.out.println(inst_Persona.Mostrar_Info_Persona()); 
+
+
+    }
+    
+}
